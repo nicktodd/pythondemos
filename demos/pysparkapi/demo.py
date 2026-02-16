09e585208ea561e988dc3106cf10bbad25aa1dee
Nick Todd <nick.todd@conygre.com>
2026-02-16 14:00:25 +0000

    minor edit for mac

--- demos/pysparkapi/demo.py
+++ demos/pysparkapi/demo.py
@@ -1,25 +1,12 @@
-from pyspark import SparkContext
 import shutil
 import os
+from pyspark import SparkContext
 
-# Clean up output folder if it exists
 if os.path.exists("lengths-output"):
-    shutil.rmtree("lengths-output")
+    shutil.rmtree("lengths-output")  # Clean up from previous runs
 
 sc = SparkContext("local[*]", "Simple PySpark Demo")
-
 inputFile = sc.textFile("Macbeth.txt")
-
 lengths = inputFile.map(lambda line: len(line))
-
-# Use collect() to avoid Hadoop file system dependencies on Windows
-results = lengths.collect()
-
-# Write output manually
-os.makedirs("lengths-output")
-with open("lengths-output/part-00000", "w") as f:
-    for length in results:
-        f.write(f"{length}\n")
-
+lengths.saveAsTextFile("lengths-output")
 sc.stop()
-print(f"Done! Processed {len(results)} lines.")
